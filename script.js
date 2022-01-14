c230b6cda47d5cd28e216683cc688467644666be
elisampaio <91321603+elisampaio@users.noreply.github.com>
2022-01-14 11:56:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ $(".item").click( function() {
   var width = $(window).width();
   var h = Math.ceil((w * 15) / 100);
   var iW = Math.ceil(($(this).height() * 100) / 100);
+  var th = $(this).height();
 
+  var size = w * 0.15;
+  console.log(th);
+  console.log(size);
   // console.log(w);
   // console.log(h);
   // console.log(iW);
   // console.log(width);
 
-    if (iW == h) {
+    if (th == size) {
       console.log('open');
       $(this).css({
         'max-height': '1500px'
