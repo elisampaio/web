16901643b9b7cb076cb9646c5642b0efa34f769e
elisampaio <91321603+elisampaio@users.noreply.github.com>
2022-01-14 11:40:47 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log('yo its working');
+
+// clicks nos .item
+$(".item").click( function() {
+
+  var w = $(window).height();
+  var width = $(window).width();
+  var h = Math.ceil((w * 15) / 100);
+  var iW = Math.ceil(($(this).height() * 100) / 100);
+
+  // console.log(w);
+  // console.log(h);
+  // console.log(iW);
+  // console.log(width);
+
+    if (iW == h) {
+      console.log('open');
+      $(this).css({
+        'max-height': '1500px'
+      });
+      $(this).children('.item-title').children('h2').children('a').css({
+        'display': 'inline-block'
+      });
+    } else {
+      $(this).css({
+        'max-height': '15vh'
+      });
+      $(this).children('.item-title').children('h2').children('a').css({
+        'display': 'none'
+      });
+    }
+
+  // if (iW == h) {
+  //   $(this).css({'max-height': '500px'});
+  //   $(this).children('.item-title').children('h2').children('a').css({	'display': 'inline-block'});
+  // }
+  // else {
+  //   $(this).css({'max-height': '15vh'});
+  //   $(this).children('.item-title').children('h2').children('a').css({	'display': 'none'});
+  // }
+
+
+});
